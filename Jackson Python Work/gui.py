fb814c9c69d37bb7a6be99d17c6ba8ddadbbac86
Jackson Curtis VonGunten <43278815+jacksonvongunten@users.noreply.github.com>
2021-11-30 13:07:53 -0500

    Initial Commit of Jackson GUI Stuff

---
+++ Jackson Python Work/gui.py
@@ -0,0 +1,122 @@
+#import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import random
+
+#to do:
+#display actors' names
+#display actors photos
+#parsing file name
+
+#map that matches distance between nodes to color
+node_dist_to_color = {
+    1: "tab:red",
+    2: "tab:orange",
+    3: "tab:olive",
+    4: "tab:green",
+    5: "tab:blue",
+    6: "tab:purple",
+}
+
+class Graph:
+
+    def __init__(self):
+        self.graph = {  }
+        self.G = nx.Graph()
+        self.colors = [ ]
+    
+    #inserts two points with a weight
+    def insert(self, n1, n2, weight):
+        if (n1 == n2):
+            return
+        if n1 not in self.graph.keys() and n2 is None:
+            self.graph[n1] = []
+            return
+        if n1 not in self.graph.keys():
+            self.graph[n1] = []
+        if n2 not in self.graph.keys():
+            self.graph[n2] = []
+        self.graph[n1].append( (n2, weight) )
+        self.graph[n2].append( (n1, weight) )
+        self.G.add_edge(n1, n2, weight=weight)
+        self.colors = ['b' for u,v in self.G.edges()]
+
+    #edges being a list/array of tuples connecting two points
+    #colors = an already exisiting array  of current edge colors
+    def highlight_path(self, edges, color):
+        for edge in edges:
+            self.change_path_color(edge, color)
+
+    #changes color of one particular edge pair 
+    #increases modularity of code 
+    #edge_pair = two points that are connected
+    #color = desired color
+    def change_path_color(self, edge_pair, color):
+        i = 0
+        for u,v in self.G.edges():
+            if (edge_pair[0] == u and edge_pair[1] == v) or (edge_pair[1] == u and edge_pair[0] == v):
+                self.colors[i] = color
+            i += 1
+        
+    #print out objects in human readable format
+    #shows adjacency list 
+    #when you use "print()" this is the function that is called
+    def __repr__(self):
+        string = ""
+        for key in self.graph.keys():
+            string += key + ": "
+            for value in self.graph[key]:
+                string += f'({value[0]}, {value[1]}), '
+            string = string[:len(string)-2]
+            string += "\n"
+        return string.rstrip()
+
+    def draw(self):
+        pos = nx.shell_layout(self.G)
+        nx.draw(self.G, pos, edge_color=self.colors, with_labels=True)
+        labels = nx.get_edge_attributes(self.G,'weight')
+        nx.draw_networkx_edge_labels(self.G,pos, edge_labels=labels)
+        plt.show()
+
+    def parse_file(self, filename):
+        with open(filename, 'r') as f:
+            lines = [line.rstrip().split(',') for line in f.readlines()]
+        for line in lines:
+            if line[0] == 'PATH':
+                path = [(line[i], line[i+1]) for i in range(1, len(line)-1)]
+                print(path)
+                self.highlight_path(path, 'r')
+            else:
+                for i in range(1, len(line)-1, 2):
+                    self.insert(line[0], line[i], line[i+1])
+
+def main():
+
+    my_graph = Graph()
+
+    my_graph.parse_file("test.txt")
+
+    my_graph.draw()
+
+if __name__ == '__main__':
+    main()
+
+
+
+'''
+31,1,0.34,2,0.14
+
+s = file.readline()
+s = s.split(',')
+['PATH', '31', '22', '64', '75', '12', '13'  ]
+(31, 22)
+(22, 64)
+(75, 64)
+
+
+=
+3921,31
+1293,33,12,34
+PATH,31,22,64,75,12
+
+'''
